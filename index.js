d4e8543a45cce201efe545ad3121555d1e3556fd
Taras Slovak <slovak.taras@gmail.com>
2022-05-02 22:09:40 +0300

    added code

--- index.js
+++ index.js
@@ -4,10 +4,26 @@ const request = require('request-promise');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const apiKey = 'fa41c7877ce0de3b5a12da617bc8e8c2';
+const baseUrl = `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
 app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('Welcome to Amazon Scraper Api.');
+});
+
+// Get product details
+app.get('/products/:productId', (req, res) => {
+  const { productId } = req.params;
+
+  try {
+    const response = await request(`${baseUrl}&url=https://www.amazon.com/dp/${productId}`);
+    
+    res.json(response)
+  } catch (error) {
+    res.json(error)
+  }
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
