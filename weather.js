49a60c3a3281bb9bb0680a1986a54f5e84a7c145
Taras Slovak <slovak.taras@gmail.com>
2022-05-22 12:31:18 +0300

    testing

--- weather.js
+++ weather.js
@@ -2,6 +2,21 @@
 import { getArgs } from "./helpers/args.js";
 import { printHelp } from "./services/log.service.js";
 import { saveKeyValue } from "./services/storage.service.js";
+import { printHelp, printSuccess, printError} from './services/log.service.js';
+
+
+async function saveToken  (token)  {
+	if (!token.length) {
+		printError('Token not passed');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
+		printSuccess('Token is saved');
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
 const initCLI = ()=>{
   const args = getArgs(process.argv); 
