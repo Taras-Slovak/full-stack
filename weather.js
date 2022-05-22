3dcb2129a87f6a5e215b7b320701130e83f6c13b
Taras Slovak <slovak.taras@gmail.com>
2022-05-22 13:34:38 +0300

    Error handling

--- weather.js
+++ weather.js
@@ -18,18 +18,47 @@ async function saveToken  (token)  {
 	}
 }
 
-const initCLI = ()=>{
-  const args = getArgs(process.argv); 
-  if (args.h) {
-    printHelp();
-  }
-  if (args.s) {
-    //...
-  }
+async function saveCity (city) {
+	if (!city.length) {
+		printError('City not assigned');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('The city is saved');
+	} catch (e) {
+		printError(e.message);
+	}
+}
 
-  if (args.t) {
-    saveKeyValue('token',args.t);
-  }
+async function getForecast ()  {
+	try {
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
+	} catch (e) {
+		if (e?.response?.status == 404) {
+			printError('Invalid city');
+		} else if (e?.response?.status == 401) {
+			printError('Invalid token');
+		} else {
+			printError(e.message);
+		}
+	}
+}
+
+const initCLI = ()=>{
+  const args = getArgs(process.argv);
+	if (args.h) {
+		return printHelp();
+	}
+	if (args.s) {
+		return saveCity(args.s);
+	}
+	if (args.t) {
+		return saveToken(args.t);
+	}
+	return getForecast ();
 }
 
 initCLI();
