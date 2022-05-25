ac9293ec88a7399e6d0168070966f5e4e58f4af3
Taras Slovak <slovak.taras@gmail.com>
2022-05-25 12:34:55 +0300

    testing

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,19 @@ import express, { Express } from "express";
 
 import { Server } from "http"
 import { userRouter } from "./users/users";
+import { LoggerService } from './logger/logger.service';
 
 export class App {
   app: Express;
   server: Server;
-  port: number
+  port: number;
+  logger: LoggerService;
 
-  constructor() {
+
+  constructor(logger: LoggerService) {
     this.app = express();
     this.port = 8000;
+    this.logger = logger;
   }
 
   useRoutes() {
@@ -20,6 +24,6 @@ export class App {
   public async init() {
     this.useRoutes();
     this.server = this.app.listen(this.port);
-    console.log(`Server started on http://localhost:${this.port}`);
+    this.logger.log(`Server started on http://localhost:${this.port}`);
   }
 }
