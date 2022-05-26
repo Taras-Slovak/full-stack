9eed53f1c429fa8d3b5f8c7b3ee7280df1bd2ed5
Taras Slovak <slovak.taras@gmail.com>
2022-05-26 10:04:58 +0300

    added baseController

---
+++ src/common/base.controller.ts
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import { LoggerService } from "../logger/logger.service";
+
+export abstract class BaseController {
+  private readonly _router = Router();
+
+  constructor(private logger: LoggerService) {
+    this._router = Router();
+  }
+
+  get router() {
+    return this._router;
+  }
+
+  protected bindRoutes(routes: any[]) {
+    this.router.get('path', func);
+  }
+}
