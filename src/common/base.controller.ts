4c1d765df8bcde1ae01e6e54dec5ae3518c5c484
Taras Slovak <slovak.taras@gmail.com>
2022-05-26 11:52:10 +0300

    added code

--- src/common/base.controller.ts
+++ src/common/base.controller.ts
@@ -1,5 +1,5 @@
 
-import { Router } from "express";
+import { Response, Router } from "express";
 import { LoggerService } from "../logger/logger.service";
 import { IControllerRoute } from "./route.interface";
 
@@ -14,7 +14,24 @@ export abstract class BaseController {
     return this._router;
   }
 
-  protected bindRoutes(routes: IControllerRoute[]) {
-    this.router.get('path', func);
+  public send<T>(res: Response, code: number, message: T) {
+    res.type('application/json');
+    return res.status(code).json(message);
+  }
+
+  public ok<T>(res: Response, message: T) {
+    return this.send<T>(res, 200, message);
+  }
+
+  public created(res: Response) {
+    return res.sendStatus(201);
+  }
+
+  protected bindRoutes(routes: IControllerRoute[]): void {
+    for (const route of routes) {
+      this.logger.log(`[${route.method}] ${route.path}`);
+      const handler = route.func.bind(this);
+      this.router[route.method](route.path, handler);
+    }
   }
 }
