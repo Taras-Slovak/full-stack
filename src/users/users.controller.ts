f12bd840dbce4c88c58a2184756fe17fb86986d8
Taras Slovak <slovak.taras@gmail.com>
2022-05-27 17:03:31 +0300

    refactored

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,5 +1,20 @@
+import { LoggerService } from './../logger/logger.service';
 import { BaseController } from './../common/base.controller';
+import { NextFunction, Request, Response } from 'express';
 
 export class UsersController extends BaseController {
+  constructor(logger: LoggerService) {
+    super(logger);
+    this.bindRoutes([
+      { path: '/register', method: 'post', func: this.register },
+      { path: '/login', method: 'post', func: this.login }
+    ])
+  }
+  login(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, 'login');
+  }
 
+  register(req: Request, res: Response, next: NextFunction) {
+    this.ok(res, 'register');
+  }
 }
