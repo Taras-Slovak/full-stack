c8e2fe55e6c8b6a4c3196848b2a9f305118d903e
Taras Slovak <slovak.taras@gmail.com>
2022-05-27 17:14:05 +0300

    refactored

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
+import { UsersController } from './users/users.controller';
 import express, { Express } from "express";
 
 import { Server } from "http"
-import { userRouter } from "./users/users";
 import { LoggerService } from './logger/logger.service';
 
 export class App {
@@ -9,16 +9,20 @@ export class App {
   server: Server;
   port: number;
   logger: LoggerService;
+  usersController: UsersController;
 
-
-  constructor(logger: LoggerService) {
+  constructor(
+    logger: LoggerService,
+    usersController: UsersController
+  ) {
     this.app = express();
     this.port = 8000;
     this.logger = logger;
+    this.usersController = usersController;
   }
 
   useRoutes() {
-    this.app.use('/users', userRouter);
+    this.app.use('/users', this.usersController.router);
   }
 
   public async init() {
