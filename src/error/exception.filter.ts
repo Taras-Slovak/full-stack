b72d4ce049b56955c293597e3fd02e060b4c6e58
Taras Slovak <slovak.taras@gmail.com>
2022-05-30 21:44:46 +0300

    refactoring

--- src/error/exception.filter.ts
+++ src/error/exception.filter.ts
@@ -1,13 +1,19 @@
 import { Request, Response, NextFunction } from "express";
 import { LoggerService } from "../logger/logger.service";
 import { IExceptionFilter } from "./exception.filter.interface";
+import { HTTPError } from "./http-error.class";
 export class exceptionFilter implements IExceptionFilter {
   logger: LoggerService;
   constructor(logger: LoggerService) {
     this.logger = logger;
   }
-  catch(err: Error, req: Request, res: Response, next: NextFunction) {
-    this.logger.error(`${err.message}`);
-    res.status(500).send({ err: err.message });
+  catch(err: Error | HTTPError, req: Request, res: Response, next: NextFunction) {
+    if (err instanceof HTTPError) {
+      this.logger.error(`[${err.context}] Error ${err.statusCode} ${err.message}`);
+      res.status(err.statusCode).send({ err: err.message });
+    } else {
+      this.logger.error(`${err.message}`)
+      res.status(500).send({ err: err.message });
+    }
   }
 }
