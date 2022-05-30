a28e9e9ca41e69938c7eaa6734d6f4704d7a16cd
Taras Slovak <slovak.taras@gmail.com>
2022-05-30 21:15:24 +0300

    refactored

--- src/error/exception.filter.ts
+++ src/error/exception.filter.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { LoggerService } from "../logger/logger.service";
-export class exceptionFilter {
+import { IExceptionFilter } from "./exception.filter.interface";
+export class exceptionFilter implements IExceptionFilter {
   logger: LoggerService;
   constructor(logger: LoggerService) {
     this.logger = logger;
