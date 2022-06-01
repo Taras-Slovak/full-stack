9cb6961850873685bb668aa295cd899d18820b74
Taras Slovak <slovak.taras@gmail.com>
2022-06-01 16:10:44 +0300

    refactored

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,21 @@
+import { Container } from 'inversify';
 import { App } from './app';
 import { ExceptionFilter } from './error/exception.filter';
+import { IExceptionFilter } from './error/exception.filter.interface';
+import { ILogger } from './logger/logger.interface';
 import { LoggerService } from './logger/logger.service';
+import { TYPES } from './types';
 import { UsersController } from './users/users.controller';
 
-async function bootstrap() {
-  const logger = new LoggerService();
-  const app = new App(
-    logger,
-    new UsersController(logger),
-    new ExceptionFilter(logger)
-  );
-  await app.init();
-}
+const appContainer = new Container();
 
-bootstrap();
+appContainer.bind<ILogger>(TYPES.ILogger).to(LoggerService);
+appContainer.bind<IExceptionFilter>(TYPES.ExceptionFilter).to(ExceptionFilter);
+appContainer.bind<UsersController>(TYPES.UsersController).to(UsersController);
+appContainer.bind<App>(TYPES.Application).to(App);
+
+const app = appContainer.get<App>(TYPES.Application);
+
+app.init();
+
+export { app, appContainer };
