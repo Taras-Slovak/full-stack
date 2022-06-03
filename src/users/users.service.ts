72ef13d7f8c1c40f1bd615d705872d152586b17b
Taras Slovak <slovak.taras@gmail.com>
2022-06-03 17:56:08 +0300

    fix

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,10 @@
+import { injectable } from 'inversify';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { User } from './user.entity';
 import { IUsersService } from './users.service.interface';
 
+@injectable()
 export class UsersService implements IUsersService {
   createUser({ email, name, password }: UserRegisterDto): User | null {
     return null;
