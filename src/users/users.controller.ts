45169318be49c08fff13df3d0100b1c7ecb2ebc1
Taras Slovak <slovak.taras@gmail.com>
2022-06-03 21:22:47 +0300

    implemented validator

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,6 +8,7 @@ import 'reflect-metadata';
 import { UserLoginDto } from './dto/user-login.dto';
 import { UserRegisterDto } from './dto/user-register.dto';
 import { UsersService } from './users.service';
+import { ValidateMiddleware } from '../common/validate.middleware';
 
 @injectable()
 export class UsersController extends BaseController {
@@ -17,7 +18,12 @@ export class UsersController extends BaseController {
   ) {
     super(loggerService);
     this.bindRoutes([
-      { path: '/register', method: 'post', func: this.register, middleware: [] },
+      {
+        path: '/register',
+        method: 'post',
+        func: this.register,
+        middleware: [new ValidateMiddleware(UserRegisterDto)],
+      },
       { path: '/login', method: 'post', func: this.login },
     ]);
   }
