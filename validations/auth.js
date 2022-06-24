819f6895ae6641620af65c08e438a6df2ca03793
Taras Slovak <slovak.taras@gmail.com>
2022-06-24 22:16:43 +0300

    fix

--- validations/auth.js
+++ validations/auth.js
@@ -2,7 +2,7 @@ import { body } from 'express-validator';
 
 export const requireValidation = [
   body('email').isEmail(),
-  body('password').isLatLong({ min: 5 }),
-  body('fullName').isLatLong({ min: 5 }),
+  body('password').isLength({ min: 5 }),
+  body('fullName').isLength({ min: 5 }),
   body('avatarUrl').optional().isURL(),
 ];
