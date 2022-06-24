438d748e5df2104baa33a03664c9e052a28c7eca
Taras Slovak <slovak.taras@gmail.com>
2022-06-24 23:36:29 +0300

    added code

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import { validationResult } from 'express-validator';
 
@@ -22,21 +23,27 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/register', requireValidation, (req, res) => {
+app.post('/auth/register', requireValidation, async (req, res) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(400).json(errors.array());
   }
 
+  const password = req.body.password;
+  const sold = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
   const doc = new UserModel({
     email: req.body.email,
     fullName: req.body.fullName,
     avatarUrl: req.body.avatarUrl,
-    passwordHash: req.body.passwordHash,
+    passwordHash,
   });
 
+  const user = await doc.save();
+
   res.json({
-    success: true,
+    user,
   });
 });
 
