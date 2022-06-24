4fb2f3ea8fe7288c7e0593075068b2f965d0d0d7
Taras Slovak <slovak.taras@gmail.com>
2022-06-24 21:21:00 +0300

    added mongoose and UserShema

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://TarasFromUa:ad3037KYZ@blog.rkhn2ff.mongodb.net/?retryWrites=true&w=majority',
+  )
+  .then(() => {
+    console.log('DB OK');
+  })
+  .catch((err) => {
+    console.log('DB error', err);
+  });
 
 const app = express();
 
@@ -10,8 +23,18 @@ app.get('/', (req, res) => {
 
 app.post('/auth/login', (req, res) => {
   console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: 'Taras Slovak',
+    },
+    'secret123',
+  );
+
   res.json({
     success: true,
+    token,
   });
 });
 
