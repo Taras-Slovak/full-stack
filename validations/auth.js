e86fe8fe253ce6e6d05200bf8bc17d8b4c7d162e
Taras Slovak <slovak.taras@gmail.com>
2022-06-24 22:21:48 +0300

    added text

--- validations/auth.js
+++ validations/auth.js
@@ -1,8 +1,11 @@
 import { body } from 'express-validator';
 
 export const requireValidation = [
-  body('email').isEmail(),
-  body('password').isLength({ min: 5 }),
-  body('fullName').isLength({ min: 5 }),
-  body('avatarUrl').optional().isURL(),
+  body('email', 'Email is not valid').isEmail(),
+  body(
+    'password',
+    'Set minimum password length to at least a value of 14',
+  ).isLength({ min: 5 }),
+  body('fullName', 'Enter a name').isLength({ min: 5 }),
+  body('avatarUrl', 'Incorrect link to avatar').optional().isURL(),
 ];
