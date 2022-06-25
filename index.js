7012d0e3dc64f73386431b872bdd179d4a0bd86c
Taras Slovak <slovak.taras@gmail.com>
2022-06-25 17:42:55 +0300

    refacrored

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ import UserModel from './models/User.js';
 
 mongoose
   .connect(
-    'mongodb+srv://TarasFromUa:ad3037KYZ@blog.rkhn2ff.mongodb.net/?retryWrites=true&w=majority',
+    'mongodb+srv://TarasFromUa:ad3037KYZ@blog.rkhn2ff.mongodb.net/blog?retryWrites=true&w=majority',
   )
   .then(() => {
     console.log('DB OK');
@@ -24,27 +24,32 @@ const app = express();
 app.use(express.json());
 
 app.post('/auth/register', requireValidation, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json(errors.array());
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json(errors.array());
+    }
+
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash,
+    });
+
+    const user = await doc.save();
+
+    res.json(user);
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      message: 'Can not register',
+    });
   }
-
-  const password = req.body.password;
-  const salt = await bcrypt.genSalt(10);
-  const passwordHash = await bcrypt.hash(password, salt);
-
-  const doc = new UserModel({
-    email: req.body.email,
-    fullName: req.body.fullName,
-    avatarUrl: req.body.avatarUrl,
-    passwordHash,
-  });
-
-  const user = await doc.save();
-
-  res.json({
-    user,
-  });
 });
 
 const port = 1456;
