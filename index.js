9c90f5459f02c641e976df3a0805b56380c1b218
Taras Slovak <slovak.taras@gmail.com>
2022-06-30 12:21:08 +0300

    refactored

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 import express from 'express';
 import 'dotenv/config';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-import { validationResult } from 'express-validator';
-
 import { requireValidation } from './validations/auth.js';
-
-import UserModel from './models/User.js';
 import checkAuth from './utils/checkAuth.js';
+import { getMe, login, register } from './controllers/UserController.js';
 
 const myPass = process.env.PASS;
 
 mongoose
   .connect(
-    `mongodb+srv://TarasFromUa:${myPass}@blog.rkhn2ff.mongodb.net/blog?retryWrites=true&w=majority`,
+    `mongodb+srv://TarasFromUa:ad3037KYZ@blog.rkhn2ff.mongodb.net/blog?retryWrites=true&w=majority`,
   )
   .then(() => {
     console.log('DB OK');
@@ -27,100 +22,11 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/auth/login', async (req, res) => {
-  try {
-    const user = await UserModel.findOne({ email: req.body.email });
+app.post('/auth/login', login);
 
-    if (!user) {
-      return res.status(403).json({
-        message: 'User not found',
-      });
-    }
-    const isValidPass = await bcrypt.compare(
-      req.body.password,
-      user._doc.passwordHash,
-    );
+app.post('/auth/register', requireValidation, register);
 
-    if (!isValidPass) {
-      return res.status(403).json({
-        message: 'Incorrect login or password',
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      { expiresIn: '30d' },
-    );
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      message: 'Can not register',
-    });
-  }
-});
-
-app.post('/auth/register', requireValidation, async (req, res) => {
-  try {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json(errors.array());
-    }
-
-    const password = req.body.password;
-    const salt = await bcrypt.genSalt(10);
-    const hash = await bcrypt.hash(password, salt);
-
-    const doc = new UserModel({
-      email: req.body.email,
-      fullName: req.body.fullName,
-      avatarUrl: req.body.avatarUrl,
-      passwordHash: hash,
-    });
-
-    const user = await doc.save();
-
-    const token = jwt.sign(
-      {
-        _id: user._id,
-      },
-      'secret123',
-      { expiresIn: '30d' },
-    );
-
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json({ ...userData, token });
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      message: 'Can not register',
-    });
-  }
-});
-
-app.get('/auth/me', checkAuth, async (req, res) => {
-  try {
-    const user = await UserModel.findById(req.userId);
-
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    const { passwordHash, ...userData } = user._doc;
-
-    res.json(userData);
-  } catch (e) {
-    console.log(e);
-    res.status(500).json({
-      message: 'No access',
-    });
-  }
-});
+app.get('/auth/me', checkAuth, getMe);
 
 const port = 1456;
 
